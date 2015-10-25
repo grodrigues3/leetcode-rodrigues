8b10b25c4fd1b82206404565d8de538e4a4742f3
Garrett Rodrigues <garrett@Garretts-MacBook-Air.local>
2015-10-24 20:13:04 -0700

    fixed flatten

--- cpp/flatten_bin_tree.cpp
+++ cpp/flatten_bin_tree.cpp
@@ -6,34 +6,6 @@
  *     TreeNode *right;
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * root)
-    
-    def make_flat(self, root):
-        #flatten the left tree and return a ptr to it
-        #flatten the right and return a ptr to it
-        if not root:
-            return
-        rl = root.left
-        rr = root.right
-        if rl is None and rr is None:
-            #leaf's are already flat
-            return root
-        lf = self.make_flat(root.left)
-        rf = self.make_flat(root.right)
-        root.left= None
-        if lf:
-            root.right = lf
-            if rf:
-                s = lf
-                #step down the left subtree
-                while s.right is not None:
-                    s = s.right
-                s.right = rf
-        elif rf:
-            # no left subtree so just point root's right subtree to flattened
-            # right tree
-            root.right = rf
-        
-        return root
  * };
  */
 class Solution {
